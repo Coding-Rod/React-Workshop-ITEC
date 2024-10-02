d8cc33c08e1b55f4beb2851a051758dd1252eb19
Coding-Rod <sergiorodrigofernandeztesta@gmail.com>
2024-10-02 01:06:53 -0400

    🚧 [ADD] useState to AddTodo.jsx

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react"
+
 export function AddTodo () {
+    const [todo, setTodo] = useState('') // Works as getter and setter
+
+    const handleAddTodo = (value) => {
+        setTodo(value) // Executes the setter
+    }
+
     return (
-        <h1>AddTodo</h1>
+        <div className="add-todo">
+            <input 
+                type="text" 
+                placeholder="Add a new todo..." 
+                value={todo} 
+                onChange={(e) => handleAddTodo(e.target.value)}
+            />
+            {/* <span>{todo}</span> */}
+            <button>Add</button>
+        </div>
     )
 }
